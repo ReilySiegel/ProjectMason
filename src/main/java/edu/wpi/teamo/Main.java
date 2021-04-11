24d47d65934921847ef7aa98f2ec977253d388ff
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-11 13:55:28 -0400

    Update package to wpi.edu.teamo (#2)
    * rename package to teamo
    
    * rename paths to teamo paths
    
    * update package in gradle main class

--- src/main/java/edu/wpi/teamo/Main.java
+++ src/main/java/edu/wpi/teamo/Main.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamname;
+package edu.wpi.teamo;
 
 public class Main {
 
--- src/test/java/edu/wpi/teamo/AppTest.java
+++ src/test/java/edu/wpi/teamo/AppTest.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamname;
+package edu.wpi.teamo;
 
 import static org.testfx.api.FxAssert.verifyThat;
 
