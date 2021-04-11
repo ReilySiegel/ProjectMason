364b0470d52d67b85f3ec3e8ceabee15075be6bf
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-11 17:59:25 -0400

    Add the Database wrapper and it's test (#8)
    * add database test
    
    * add reily's database
    Co-authored-by: Reily Siegel mail@reilysiegel.com
    
    * literally add a newline
    
    * close connection to database in DatabaseTest.testConnectDatabase

--- src/main/java/edu/wpi/teamo/map/database/Database.java
+++ src/main/java/edu/wpi/teamo/map/database/Database.java
@@ -1,5 +1,32 @@
 package edu.wpi.teamo.map.database;
 
-public class Database {
+import java.sql.*;
 
+public class Database {
+    Connection conn;
+
+    public Database () throws SQLException, ClassNotFoundException {
+        this("jdbc:derby:memory:DB;create=true");
+    }
+
+    public Database (String uri) throws SQLException, ClassNotFoundException {
+        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        conn = DriverManager.getConnection(uri);
+    }
+
+    public ResultSet processQuery (String s) throws SQLException {
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(s);
+        return rs;
+    }
+
+    public int processUpdate (String s) throws SQLException {
+        Statement stmt = conn.createStatement();
+        int numUpdated = stmt.executeUpdate(s);
+        return numUpdated;
+    }
+
+    public void close() throws SQLException {
+        conn.close();
+    }
 }
---
+++ src/test/java/edu/wpi/teamo/map/database/DatabaseTest.java
@@ -0,0 +1,21 @@
+package edu.wpi.teamo.map.database;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseTest {
+
+    @Test
+    public void testConnectDatabase() {
+        boolean pass = true;
+        try {
+            Database db = new Database();
+            db.close();
+        } catch (Exception e) {
+            System.out.println("CANNOT CONNECT TO DATABASE");
+            e.printStackTrace();
+            pass = false;
+        }
+        assertTrue(pass);
+    }
+}
