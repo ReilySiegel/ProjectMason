221604c9058cdcef7809d67e13a6c06f1c3bfe79
FinalHalation <64982897+FinalHalation@users.noreply.github.com>
2021-04-11 16:08:05 -0400

    added infrastructure for switching pages (#3)
    Co-authored-by: Mason <mvpowell@wpi.edu>

---
+++ src/main/java/edu/wpi/teamo/views/SR07_Medicine.java
@@ -0,0 +1,14 @@
+package edu.wpi.teamo.views;
+
+import edu.wpi.teamo.App;
+import java.io.IOException;
+
+import edu.wpi.teamo.Pages;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public class SR07_Medicine {
+
+}
