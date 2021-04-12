3882264544a949fd82bb1ee4acd538ca0d34f219
FinalHalation <64982897+FinalHalation@users.noreply.github.com>
2021-04-12 18:45:54 -0400

    basic edge page design down (#46)
    Co-authored-by: Mason <mvpowell@wpi.edu>

--- src/main/java/edu/wpi/teamo/views/EdgePage.java
+++ src/main/java/edu/wpi/teamo/views/EdgePage.java
@@ -1,5 +1,7 @@
 package edu.wpi.teamo.views;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import edu.wpi.teamo.App;
 import java.io.IOException;
 
@@ -8,7 +10,70 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 
 public class EdgePage {
+    @FXML
+    private JFXTextField addEdgeID;
+
+    @FXML
+    private JFXTextField addNode1;
+
+    @FXML
+    private JFXTextField addNode2;
+
+    @FXML
+    private JFXButton addSubmit;
+
+    @FXML
+    private JFXTextField editingEdge;
+
+    @FXML
+    private Label currentEdge;
+
+    @FXML
+    private JFXTextField editEdgeID;
+
+    @FXML
+    private JFXTextField editNode1;
+
+    @FXML
+    private JFXTextField editNode2;
+
+    @FXML
+    private JFXButton editSubmit;
+
+    @FXML
+    private JFXTextField deleteEdgeID;
+
+    @FXML
+    private JFXButton deleteSubmit;
+
+    @FXML
+    void handleAddSubmit(ActionEvent event) {
+        String newEdgeID = addEdgeID.getText();
+        String newEdgeNode1 = addNode1.getText();
+        String newEdgeNode2 = addNode2.getText();
+
+    }
+
+    @FXML
+    void handleDeleteSubmit(ActionEvent event) {
+        String deleteEdge = deleteEdgeID.getText();
+
+    }
+
+    @FXML
+    void handleEditSubmit(ActionEvent event) {
+        String currentEdgeID = editingEdge.getText();
+        String neweditEdgeID = editEdgeID.getText();
+        String editEdgeNode1 = editNode1.getText();
+        String newEdgeNode2 = editNode2.getText();
+    }
+
+    @FXML
+    void handleBacktoMain(ActionEvent event) {
+        App.switchPage(Pages.MAIN);
+    }
 
 }
