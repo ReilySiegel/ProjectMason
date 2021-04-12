48be39f949b7882c43f2f8c51b249ccb8a8c734e
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-11 20:51:35 -0400

    MapStorage interface for everyone to call (#15)
    * add MapStorage interface
    
    * added getters
    
    * word wrap
    
    * no set edge name

--- src/main/java/edu/wpi/teamo/map/database/MapStorage.java
+++ src/main/java/edu/wpi/teamo/map/database/MapStorage.java
@@ -2,4 +2,21 @@ package edu.wpi.teamo.map.database;
 
 public interface MapStorage {
 
+    /* the getters */
+    NodeInfo getNode(String id);
+    EdgeInfo getEdge(String id);
+
+    /* the setters */
+    boolean setNodePosition(String id, int newX, int newY);
+    boolean setNodeLongName(String id, String name);
+
+    /* the makers */
+    boolean addEdge(String edgeID, String startNodeID, String endNodeID);
+    boolean addNode(String nodeID, int xPos, int yPos, String floor,
+                    String building, String nodeType, String longName, String shortNam);
+
+    /* the takers */
+    boolean deleteNode(String id);
+    boolean deleteEdge(String id);
+
 }
