6ed57e8e6ba7222fae8d911d213ea0f859192c36
Nestor <nlzx432@gmail.com>
2021-04-12 17:44:31 -0400

    test Node.getByID

--- src/test/java/edu/wpi/teamo/map/database/NodeTests.java
+++ src/test/java/edu/wpi/teamo/map/database/NodeTests.java
@@ -18,9 +18,41 @@ public class NodeTests {
 
     @Test
     public void testGetByID() {
-        boolean pass = true;
-        //TODO: Finish
-        assertTrue(pass);
+        String building = "building";
+        String shortName = "short";
+        String longName = "long";
+        String nodeID = "testID";
+        String floor = "floor";
+        String type = "type";
+        int xPos = 1;
+        int yPos = 2;
+
+        try {
+            Database db = new Database(generateURIFromName("GetNodeByID"));
+            Node.initTable(db);
+
+            /* store an edge */
+            Node nodeToStore = new Node(nodeID, xPos, yPos, floor, building, type, longName, shortName);
+            nodeToStore.update(db);
+
+            /* fetch it */
+            Node nodeToCheck = Node.getByID(db, nodeID);
+
+            /* check if its the same */
+            assertEquals(shortName, nodeToCheck.getShortName());
+            assertEquals(longName, nodeToCheck.getLongName());
+            assertEquals(building, nodeToCheck.getBuilding());
+            assertEquals(type, nodeToCheck.getNodeType());
+            assertEquals(nodeID, nodeToCheck.getNodeID());
+            assertEquals(floor, nodeToCheck.getFloor());
+            assertEquals(xPos, nodeToCheck.getXPos());
+            assertEquals(yPos, nodeToCheck.getYPos());
+
+            db.close();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
     }
 
     @Test
