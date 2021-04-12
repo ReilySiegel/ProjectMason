3646507b6260304ee8571184ba6e3727e886db3e
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-12 11:48:28 -0400

    add node test stubs (#24)
    * add node test stubs
    
    * newline

---
+++ src/test/java/edu/wpi/teamo/map/database/NodeTests.java
@@ -0,0 +1,35 @@
+package edu.wpi.teamo.map.database;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NodeTests {
+
+    @Test
+    public void testInitTable() {
+        boolean pass = true;
+        //TODO: Finish
+        assertTrue(pass);
+    }
+
+    @Test
+    public void testGetByID() {
+        boolean pass = true;
+        //TODO: Finish
+        assertTrue(pass);
+    }
+
+    @Test
+    public void testGetAll() {
+        boolean pass = true;
+        //TODO: Finish
+        assertTrue(pass);
+    }
+
+    @Test
+    public void testUpdate() {
+        boolean pass = true;
+        //TODO: Finish
+        assertTrue(pass);
+    }
+}
