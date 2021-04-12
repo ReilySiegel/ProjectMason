1b62f65bfd748f47c85f69ada9bc499368fdf8d5
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-11 20:02:24 -0400

    Foundation for the CSV IO classes (stubs) (#19)
    * start NodeCSV
    
    * fix typo
    
    * foundation for edge CSV

---
+++ src/main/java/edu/wpi/teamo/map/database/EdgeCSV.java
@@ -0,0 +1,20 @@
+package edu.wpi.teamo.map.database;
+
+import java.util.HashMap;
+
+public class EdgeCSV {
+
+    public static HashMap<String, Edge> read(String filename) {
+        HashMap<String, Edge> edgeMap = new HashMap<>();
+        //TODO: finish
+        return edgeMap;
+    }
+
+    public static boolean write(String filename, HashMap<String, Edge> edgeMap) {
+        boolean written = true;
+        //TODO: finish
+        return written;
+    }
+
+}
+
---
+++ src/main/java/edu/wpi/teamo/map/database/NodeCSV.java
@@ -0,0 +1,19 @@
+package edu.wpi.teamo.map.database;
+
+import java.util.HashMap;
+
+public class NodeCSV {
+
+    public static HashMap<String, Node> read(String filename) {
+        HashMap<String, Node> nodeMap = new HashMap<>();
+        //TODO: finish
+        return nodeMap;
+    }
+
+    public static boolean write(String filename, HashMap<String, Node> nodeMap) {
+        boolean written = true;
+        //TODO: finish
+        return written;
+    }
+
+}
