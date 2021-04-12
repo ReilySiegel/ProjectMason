efcade45be10354616b7480b94697e9ce807213d
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-12 16:47:24 -0400

    Test Edge.getAll (#29)
    * Test Edge.getAll
    
    * use fail

--- src/test/java/edu/wpi/teamo/map/database/EdgeTests.java
+++ src/test/java/edu/wpi/teamo/map/database/EdgeTests.java
@@ -1,6 +1,12 @@
 package edu.wpi.teamo.map.database;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EdgeTests {
@@ -21,9 +27,44 @@ public class EdgeTests {
 
     @Test
     public void testGetAll() {
-        boolean pass = true;
-        //TODO: Finish
-        assertTrue(pass);
+        String edge1ID = "testIDOne";
+        String edge2ID = "testIDTwo";
+        String edge3ID = "testIDTre";
+
+        try {
+            Database db = new Database();
+            Edge.initTable(db);
+
+            /* store three edges */
+            Edge eStoredOne = new Edge(edge1ID, edge1ID + "start", edge1ID + "end");
+            Edge eStoredTwo = new Edge(edge2ID, edge2ID + "start", edge2ID + "end");
+            Edge eStoredTre = new Edge(edge3ID, edge3ID + "start", edge3ID + "end");
+            eStoredOne.update(db);
+            eStoredTwo.update(db);
+            eStoredTre.update(db);
+
+            /* fetch all - into list */
+            Stream<Edge> edgeStream = Edge.getAll(db);
+            List<Edge>   edgeList   = edgeStream.collect(Collectors.toList());
+
+            /* list must be length 3 */
+            int size = edgeList.size();
+            assertEquals(3, size);
+            if (size == 3) {
+                /* make list of the IDs */
+                ArrayList<String> ids = new ArrayList<>();
+                for (int i = 0; i < 3; i++) ids.add(edgeList.get(i).getEdgeID());
+
+                /* check if ids are the same */
+                assertTrue(ids.contains(edge1ID));
+                assertTrue(ids.contains(edge2ID));
+                assertTrue(ids.contains(edge3ID));
+            }
+
+            db.close();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
