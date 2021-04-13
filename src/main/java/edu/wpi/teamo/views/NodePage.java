ab942f5774ef2ead3cd8286a57a76eb4fb12b2f6
Sprowe <81380946+Sprowe@users.noreply.github.com>
2021-04-13 19:36:50 -0400

    Tied each text field and button to functions in the Node Page controller (#59)

--- src/main/java/edu/wpi/teamo/views/NodePage.java
+++ src/main/java/edu/wpi/teamo/views/NodePage.java
@@ -1,5 +1,7 @@
 package edu.wpi.teamo.views;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import edu.wpi.teamo.App;
 import java.io.IOException;
 
@@ -8,10 +10,70 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 
 public class NodePage {
 
+    @FXML
+    private JFXTextField addNodeID;
 
+    @FXML
+    private JFXTextField addNodeX;
+
+    @FXML
+    private JFXTextField addNodeY;
+
+    @FXML
+    private JFXButton addSubmit;
+
+    @FXML
+    private JFXTextField origNodeID;
+
+    @FXML
+    private JFXTextField origNodeX;
+
+    @FXML
+    private JFXTextField origNodeY;
+
+    @FXML
+    private JFXTextField editNodeID;
+
+    @FXML
+    private JFXTextField editNodeX;
+
+    @FXML
+    private JFXTextField editNodeY;
+
+    @FXML
+    private JFXButton editSubmit;
+
+    @FXML
+    private JFXTextField deleteNodeID;
+
+    @FXML
+    private JFXButton deleteSubmit;
+
+    @FXML
+    void handleAddSubmit(ActionEvent event) {
+        String newNodeID = addNodeID.getText();
+        String newNodeX = addNodeX.getText();
+        String newNodeY = addNodeY.getText();
+
+    }
+
+    @FXML
+    void handleDeleteSubmit(ActionEvent event) {
+        String deleteNode = deleteNodeID.getText();
+
+    }
+
+    @FXML
+    void handleEditSubmit(ActionEvent event) {
+        String currentNodeID = origNodeID.getText();
+        String newNodeID = editNodeID.getText();
+        String newNodeX = editNodeX.getText();
+        String newNodeY = editNodeY.getText();
+    }
     /**
      * event hanlder for switching to main page
      * @param e Action Event parameter
