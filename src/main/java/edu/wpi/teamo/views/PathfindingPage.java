e411264bf84de191c43582e12a821521dd0cf8ad
Rushab Patil <38635683+SpaceDranzer@users.noreply.github.com>
2021-04-14 11:12:54 -0400

    Path finding page - Get Directions Button Setup (#78)
    * Created Path Finding Page
    
    * Added Get Directions Button and handler function
    
    * ConnectedStartLocation and EndLocation MenuButton with Controller

--- src/main/java/edu/wpi/teamo/views/PathfindingPage.java
+++ src/main/java/edu/wpi/teamo/views/PathfindingPage.java
@@ -1,5 +1,7 @@
 package edu.wpi.teamo.views;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import edu.wpi.teamo.App;
 import java.io.IOException;
 
@@ -8,7 +10,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 
 public class PathfindingPage {
 
+    @FXML
+    private JFXButton getDirections;
+
+    @FXML
+    private MenuButton startLocation;
+
+    @FXML
+    private MenuButton endLocation;
+
+
+
+    @FXML
+    void handleGetDirections(ActionEvent event) {
+
+        String startNodeId;
+        String endNodeId;
+
+
+    }
 }
