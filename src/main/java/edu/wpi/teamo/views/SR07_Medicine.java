5cb333d76f75358a031c06256f213d6abfe0940a
duffelmuffinvii <30965453+duffelmuffinvii@users.noreply.github.com>
2021-04-14 17:44:42 -0400

    Controller shenanigans for medicine and sanitation pages (#100)
    * Added fields in controllers and switched dropdown to jfx version in sanitation
    
    * location doesnt work as a name i guess
    
    * beginning of submission handler

--- src/main/java/edu/wpi/teamo/views/SR07_Medicine.java
+++ src/main/java/edu/wpi/teamo/views/SR07_Medicine.java
@@ -1,17 +1,29 @@
 package edu.wpi.teamo.views;
 
+import com.jfoenix.controls.JFXTextField;
 import edu.wpi.teamo.App;
-
-import java.awt.*;
-import java.io.IOException;
 
 import edu.wpi.teamo.Pages;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 
 public class SR07_Medicine {
+
+    @FXML
+    private JFXTextField medName;
+
+    @FXML
+    private JFXTextField room;
+
+    @FXML
+    private JFXTextField userName;
+
+    @FXML
+    private void handleSubmission(ActionEvent e) {
+        String medicine = medName.getText();
+        String roomNumber = room.getText().replaceAll("[^a-zA-Z0-9]", "");
+        String name = userName.getText().replaceAll("[^a-zA-Z0-9]", "");
+    }
 
     @FXML
     private void backToMain(ActionEvent e) {
--- src/main/java/edu/wpi/teamo/views/Sanitation.java
+++ src/main/java/edu/wpi/teamo/views/Sanitation.java
@@ -1,11 +1,33 @@
 package edu.wpi.teamo.views;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTimePicker;
 import edu.wpi.teamo.App;
 import edu.wpi.teamo.Pages;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class Sanitation {
+
+    @FXML
+    private JFXTextField service;
+
+    @FXML
+    private JFXTimePicker serviceTime;
+
+    @FXML
+    private JFXTimePicker estJobTime;
+
+    @FXML
+    private JFXComboBox serviceLocation;
+
+    @FXML
+    private JFXTextField priority;
+
+    @FXML
+    private JFXTextArea notes;
 
     @FXML
     private void backToMain(ActionEvent e) {
