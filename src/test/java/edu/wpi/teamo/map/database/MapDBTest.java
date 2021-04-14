ed9740a66bc3c132646bd6eafa34baaaa7e66fb1
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-14 02:32:00 -0400

    Test Load/Write interface functions (#68)
    * add MapDBTest.testLoadFromCSVs
    
    * add MapDBTest.testWriteEdgesToCSV
    
    * test WriteNodesToCSV

--- src/test/java/edu/wpi/teamo/map/database/MapDBTest.java
+++ src/test/java/edu/wpi/teamo/map/database/MapDBTest.java
@@ -1,26 +1,29 @@
 package edu.wpi.teamo.map.database;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @TestInstance(Lifecycle.PER_CLASS)
 public class MapDBTest {
+
+    static final String testNodeFile = "src/test/resources/edu/wpi/teamo/map/database/testNodes.csv";
+    static final String testEdgeFile = "src/test/resources/edu/wpi/teamo/map/database/testEdges.csv";
 
     MapDB db;
 
     @BeforeAll
     public void setup() {
         try {
-            db = new MapDB("src/test/resources/edu/wpi/teamo/map/database/testNodes.csv",
-                    "src/test/resources/edu/wpi/teamo/map/database/testEdges.csv");
+            db = new MapDB(testNodeFile, testEdgeFile);
         } catch (Exception e) {
 
         }
@@ -74,4 +77,66 @@ public class MapDBTest {
         assertNull(db.getEdge("edgeID1"));
     }
 
+    @Test
+    public void testLoadFromCSVs() {
+        MapDB tMDB = new MapDB("testLoadFromCSVs");
+        try {
+
+            tMDB.loadEdgesFromFile(testEdgeFile);
+            tMDB.loadNodesFromFile(testNodeFile);
+
+            Stream<NodeInfo> nodeStream = tMDB.getAllNodes();
+            assertEquals(3, nodeStream.count());
+
+            Stream<EdgeInfo> edgeStream = tMDB.getAllEdges();
+            assertEquals(3, edgeStream.count());
+
+        } catch (FileNotFoundException e) {
+            fail("Cannot testLoadFromCSVs, file not found.");
+        }
+    }
+
+    @Test
+    public void testWriteEdgesToCSV() {
+        final String writtenFilepath = "src/test/resources/edu/wpi/teamo/map/database/writtenEdges.csv";
+        MapDB tMDB = new MapDB("testWriteEdgesToCSVs");
+        try {
+
+            tMDB.loadEdgesFromFile(testEdgeFile);
+            tMDB.writeEdgesToCSV(writtenFilepath);
+            HashMap<String, Edge> edgesToCheck = EdgeCSV.read(writtenFilepath);
+
+            assertEquals(3, edgesToCheck.size());
+            assertEquals("edge1E", edgesToCheck.get("edgeID1").getEndNodeID());
+            assertEquals("edge2E", edgesToCheck.get("edgeID2").getEndNodeID());
+            assertEquals("edge3E", edgesToCheck.get("edgeID3").getEndNodeID());
+
+            new File(writtenFilepath).delete();
+
+        } catch (FileNotFoundException e) {
+            fail("Cannot testLoadFromCSVs, file not found.");
+        }
+    }
+
+    @Test
+    public void testWriteNodesToCSV() {
+        final String writtenFilepath = "src/test/resources/edu/wpi/teamo/map/database/writtenNodes.csv";
+        MapDB tMDB = new MapDB("testWriteNodesToCSVs");
+        try {
+
+            tMDB.loadNodesFromFile(testNodeFile);
+            tMDB.writeNodesToCSV(writtenFilepath);
+            HashMap<String, Node> nodesToCheck = NodeCSV.read(writtenFilepath);
+
+            assertEquals(3, nodesToCheck.size());
+            assertEquals("Floor1RightParking1", nodesToCheck.get("testID1").getLongName());
+            assertEquals("Floor2RightParking2", nodesToCheck.get("testID2").getLongName());
+            assertEquals("Floor3RightParking3", nodesToCheck.get("testID3").getLongName());
+
+            new File(writtenFilepath).delete();
+
+        } catch (FileNotFoundException e) {
+            fail("Cannot testLoadFromCSVs, file not found.");
+        }
+    }
 }
