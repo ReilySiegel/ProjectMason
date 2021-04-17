2b75f51c89981a22b6439b9b50788308a4335f9a
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-17 02:08:11 -0400

    MapCSV stubs (#4)
    * add MapCSV class
    
    * MapCSV.write
    
    * MapCSV.readNodes
    
    * MapCSV.readEdges
    
    * MapCSV.readEdges

---
+++ src/main/java/edu/wpi/teamo/database/map/MapCSV.java
@@ -0,0 +1,22 @@
+package edu.wpi.teamo.database.map;
+
+import java.util.stream.Stream;
+
+public class MapCSV {
+
+    public static void write(String filepath, Stream<Node> nodeStream, Stream<Edge> edgeStream) {
+        //TODO
+    }
+
+    public static Stream<Node> readNodes(String filepath) {
+        //TODO
+        return null;
+    }
+
+    public static Stream<Edge> readEdges(String filepath) {
+        //TODO
+        return null;
+    }
+
+}
+
