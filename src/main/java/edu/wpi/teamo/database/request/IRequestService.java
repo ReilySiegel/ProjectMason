fe69dcd3ed28e41ed34fec7056e4cf5eebd58635
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-17 18:27:04 -0400

    Make the IRequestService methods throw SQL's (#22)
    * IRequestService throws SQL
    
    * RequestDB throws SQL

--- src/main/java/edu/wpi/teamo/database/request/IRequestService.java
+++ src/main/java/edu/wpi/teamo/database/request/IRequestService.java
@@ -1,15 +1,16 @@
 package edu.wpi.teamo.database.request;
 
 import java.util.stream.Stream;
+import java.sql.SQLException;
 
 public interface IRequestService {
-    String requestMedicine(String type, String amount, String location, String employee, int requestNumber);
-    String requestSanitation(String type, String location, String employee, int requestNumber);
-    Stream<ISanitationRequestInfo> getAllSanitationRequests();
-    Stream<IMedicineRequestInfo> getAllMedicineRequests();
-    ISanitationRequestInfo getSanitationRequest(String id);
-    IMedicineRequestInfo getMedicineRequest(String id);
-    void removeRequest(String requestID);
-    void setCompleted(String requestID);
-    void closeConnection();
+    String requestMedicine(String type, String amount, String location, String employee, int requestNumber) throws SQLException;
+    String requestSanitation(String type, String location, String employee, int requestNumber) throws SQLException;
+    Stream<ISanitationRequestInfo> getAllSanitationRequests() throws SQLException;
+    Stream<IMedicineRequestInfo> getAllMedicineRequests() throws SQLException;
+    ISanitationRequestInfo getSanitationRequest(String id) throws SQLException;
+    IMedicineRequestInfo getMedicineRequest(String id) throws SQLException;
+    void removeRequest(String requestID) throws SQLException;
+    void setCompleted(String requestID) throws SQLException;
+    void closeConnection() throws SQLException;
 }
--- src/main/java/edu/wpi/teamo/database/request/RequestDB.java
+++ src/main/java/edu/wpi/teamo/database/request/RequestDB.java
@@ -3,62 +3,63 @@ package edu.wpi.teamo.database.request;
 import edu.wpi.teamo.database.Database;
 
 import java.util.stream.Stream;
+import java.sql.SQLException;
 
 public class RequestDB implements IRequestService {
     Database db;
 
-    public RequestDB(Database db) {
+    public RequestDB(Database db) throws SQLException {
         this.db = db;
         SanitationRequest.initTable(db);
         MedicineRequest.initTable(db);
     }
 
     @Override
-    public String requestMedicine(String type, String amount, String location, String employee, int requestNumber) {
+    public String requestMedicine(String type, String amount, String location, String employee, int requestNumber) throws SQLException {
         //TODO
         return null;
     }
 
     @Override
-    public String requestSanitation(String type, String location, String employee, int requestNumber) {
+    public String requestSanitation(String type, String location, String employee, int requestNumber) throws SQLException {
         //TODO
         return null;
     }
 
     @Override
-    public Stream<ISanitationRequestInfo> getAllSanitationRequests() {
+    public Stream<ISanitationRequestInfo> getAllSanitationRequests() throws SQLException {
         //TODO
         return null;
     }
 
     @Override
-    public Stream<IMedicineRequestInfo> getAllMedicineRequests() {
+    public Stream<IMedicineRequestInfo> getAllMedicineRequests() throws SQLException {
         //TODO
         return null;
     }
 
     @Override
-    public ISanitationRequestInfo getSanitationRequest(String id) {
+    public ISanitationRequestInfo getSanitationRequest(String id) throws SQLException {
         return null;
     }
 
     @Override
-    public IMedicineRequestInfo getMedicineRequest(String id) {
+    public IMedicineRequestInfo getMedicineRequest(String id) throws SQLException {
         return null;
     }
 
     @Override
-    public void removeRequest(String requestID) {
+    public void removeRequest(String requestID) throws SQLException {
         //TODO
     }
 
     @Override
-    public void setCompleted(String requestID) {
+    public void setCompleted(String requestID) throws SQLException {
         //TODO
     }
 
     @Override
-    public void closeConnection() {
+    public void closeConnection() throws SQLException {
         //TODO
     }
 }
