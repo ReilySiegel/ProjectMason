2bad7ec2a70fe362b34e321433fbc60bd027a49d
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-17 04:06:01 -0400

    RequestDB foundation (#10)
    * Added RequestDB clas
    
    * RequestDB foundation

---
+++ src/main/java/edu/wpi/teamo/database/request/RequestDB.java
@@ -0,0 +1,54 @@
+package edu.wpi.teamo.database.request;
+
+import edu.wpi.teamo.database.Database;
+
+import java.util.stream.Stream;
+
+public class RequestDB implements IRequestService {
+    Database db;
+
+    public RequestDB(Database db) {
+        this.db = db;
+        SanitationRequest.initTable(db);
+        MedicineRequest.initTable(db);
+    }
+
+    @Override
+    public String requestMedicine(String type, String amount, String location, String employee, int requestNumber) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public String requestSanitation(String type, String location, String employee, int requestNumber) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Stream<ISanitationRequestInfo> getAllSanitationRequests() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Stream<IMedicineRequestInfo> getAllMedicineRequests() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void removeRequest(String requestID) {
+        //TODO
+    }
+
+    @Override
+    public void setCompleted(String requestID) {
+        //TODO
+    }
+
+    @Override
+    public void closeConnection() {
+        //TODO
+    }
+}
