633069d0fc845e7ce1d7de5f584e4710900230cb
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2021-04-17 04:30:47 -0400

    Service instantiation (#12)
    * Request service instantiation in App
    
    * Request service instantiation in App

--- src/main/java/edu/wpi/teamo/App.java
+++ src/main/java/edu/wpi/teamo/App.java
@@ -1,9 +1,12 @@
 package edu.wpi.teamo;
 
+import edu.wpi.teamo.database.request.IRequestService;
+import edu.wpi.teamo.database.request.RequestDB;
+import edu.wpi.teamo.database.map.IMapService;
 import edu.wpi.teamo.algos.AStarManager;
 import edu.wpi.teamo.algos.AStarService;
-import edu.wpi.teamo.database.map.IMapService;
 import edu.wpi.teamo.database.map.MapDB;
+import edu.wpi.teamo.database.Database;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -17,34 +20,39 @@ import java.util.EnumMap;
 public class App extends Application {
 
   private static final EnumMap<Pages, String> pagePaths = new EnumMap<>(Pages.class);
-  public static IMapService dbService = null;
+  public static IRequestService requestService = null;
   public static AStarService aStarService = null;
+  public static IMapService mapService = null;
+  public static IMapService dbService = null; // <- TODO legacy symbol, remove when no longer used
   private static Stage primaryStage;
 
 
   @Override
   public void init() {
+    pagePaths.put(Pages.PATHFINDING, "/edu/wpi/teamo/fxml/PathfindingPage.fxml");
     pagePaths.put(Pages.MEDICINE, "/edu/wpi/teamo/fxml/SR07_Medicine.fxml");
+    pagePaths.put(Pages.SANITATION, "/edu/wpi/teamo/fxml/Sanitation.fxml");
     pagePaths.put(Pages.NODE, "/edu/wpi/teamo/fxml/NodePage.fxml");
     pagePaths.put(Pages.EDGE, "/edu/wpi/teamo/fxml/EdgePage.fxml");
-    pagePaths.put(Pages.PATHFINDING, "/edu/wpi/teamo/fxml/PathfindingPage.fxml");
     pagePaths.put(Pages.MAIN, "/edu/wpi/teamo/fxml/MainPage.fxml");
-    pagePaths.put(Pages.SANITATION, "/edu/wpi/teamo/fxml/Sanitation.fxml");
     System.out.println("Starting Up");
 
-    /* instantiate the map service, set to a static variable that can be accessed from the handlers */
+    /* instantiate the database services, set to static variables that can be accessed from the handlers */
     try {
-      dbService = new MapDB();
-      System.out.println("Database Initialized");
+      Database db = new Database();
+      requestService = new RequestDB(db);
+      mapService = new MapDB(db);
+      dbService = mapService; //see declaration
+      System.out.println("Database Services Initialized");
     } catch (SQLException | ClassNotFoundException e) {
-      System.out.println("ERROR: FAILED TO INIT DATABASE");
+      System.out.println("ERROR: FAILED TO INIT DATABASE SERVICES");
       e.printStackTrace();
       dbService = null;
     }
 
     /* instantiate the aStar service, set to a static variable that can be accessed from the handlers */
-    if (dbService != null) {
-      aStarService = new AStarManager(dbService);
+    if (mapService != null) {
+      aStarService = new AStarManager(mapService);
       System.out.println("Pathfinder Service Initialized");
     }
   }
