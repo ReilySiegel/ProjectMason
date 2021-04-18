a923f7c5ffb2120474fd8956e0c4e5522e372868
Reily Siegel <mail@reilysiegel.com>
2021-04-17 20:19:28 -0400

    Persist database by default (#24)
    Changes the default JDBC URI to make a directory for the database by default.
    This default directory is $HOME/.oxblood/db.

--- src/main/java/edu/wpi/teamo/database/Database.java
+++ src/main/java/edu/wpi/teamo/database/Database.java
@@ -1,12 +1,17 @@
 package edu.wpi.teamo.database;
 
+import java.nio.file.Paths;
 import java.sql.*;
 
 public class Database {
     Connection conn;
 
+    private static String defaultFilePath () {
+        return Paths.get(System.getProperty("user.home"), ".oxblood", "db").toString();
+    }
+
     public Database () throws SQLException, ClassNotFoundException {
-        this("jdbc:derby:memory:DB;create=true");
+        this("jdbc:derby:" + defaultFilePath() +  ";create=true");
     }
 
     public Database (String uri) throws SQLException, ClassNotFoundException {
@@ -21,9 +26,17 @@ public class Database {
     }
 
     public int processUpdate (String s) throws SQLException {
-        Statement stmt = conn.createStatement();
-        int numUpdated = stmt.executeUpdate(s);
-        return numUpdated;
+        try {
+            Statement stmt = conn.createStatement();
+            int numUpdated = stmt.executeUpdate(s);
+            return numUpdated;
+        } catch (SQLException e) {
+            // Ignore if table already exists in the DB.
+            if (e.getSQLState().equals("X0Y32"))
+                return 0;
+            else
+                throw e;
+        }
     }
 
     public void close() throws SQLException {
