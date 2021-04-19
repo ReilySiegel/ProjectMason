873498aa331de894b7d0964833b85e68a8966920
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2021-04-18 21:15:41 -0400

    Temporary hotfix for handling invalid/unspecified node types. (#66)

--- src/main/java/edu/wpi/teamo/algos/NodeType.java
+++ src/main/java/edu/wpi/teamo/algos/NodeType.java
@@ -4,6 +4,6 @@ package edu.wpi.teamo.algos;
  * enumerations to signify the type of AlgoNode based on the type in the CSV files
  */
 public enum NodeType {
-    CONF,DEPT,HALL,LABS,REST,SERV,ELEV,PARK,WALK;
+    CONF,DEPT,HALL,LABS,REST,SERV,ELEV,PARK,WALK,NULL
 }
 
