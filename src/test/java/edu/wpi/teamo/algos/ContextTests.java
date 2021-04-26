d28399961481e439067fe7d7d1cb95b8ba3abe79
Oliver Chen <75399499+carrier-of-cyberbully@users.noreply.github.com>
2021-04-26 20:18:55 +0800

    HOT FIX!!!!! (#182)
    Someone plz approve it!!!

--- src/test/java/edu/wpi/teamo/algos/ContextTests.java
+++ src/test/java/edu/wpi/teamo/algos/ContextTests.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class DFSManagerTests {
+public class ContextTests {
+
     @Test
     public void testGetPath() throws SQLException, ClassNotFoundException {
         // make to have different databaseName
-        MapDB mdb = new MapDB("testFindPath1");
+        MapDB mdb = new MapDB("testFindPath2");
         mdb.addNode("oPARK00101", 3116,1131,"F1", "b","PARK","Floor1RightParking1","F1RightP1");
         mdb.addNode("oPARK00201", 3116,1155,"F1", "b","PARK","Floor1RightParking2","F1RightP2");
         mdb.addNode("oPARK00301", 3116,1181,"F1", "b","PARK","Floor1RightParking3","F1RightP3");
@@ -36,8 +36,11 @@ public class DFSManagerTests {
         mdb.addEdge("oPARK00801_oPARK00901","oPARK00801","oPARK00901");
         mdb.addEdge("oPARK00801_oPARK01001","oPARK00801","oPARK01001");
 
-        DFSManager dsm = new DFSManager(mdb);
-        LinkedList<AlgoNode> path_temp = dsm.getPath("oPARK01001","oPARK00501");
+
+        // Contest using dFS
+        DFSManager dfs = new DFSManager(mdb);
+        Context context = new Context(dfs);
+        LinkedList<AlgoNode> path_temp = context.getPath("oPARK01001","oPARK00501");
 
         assertEquals(7, path_temp.size());
         assertEquals("oPARK01001", path_temp.get(0).getID());
@@ -49,28 +52,21 @@ public class DFSManagerTests {
         assertEquals("oPARK00501", path_temp.get(6).getID());
 
 
-        LinkedList<AlgoNode> path_temp1 = dsm.getPath("oPARK00701","oPARK00501");
-        assertEquals(5, path_temp1.size());
-        assertEquals("oPARK00701", path_temp1.get(0).getID());
-        assertEquals("oPARK00201", path_temp1.get(1).getID());
-        assertEquals("oPARK00101", path_temp1.get(2).getID());
-        assertEquals("oPARK00601", path_temp1.get(3).getID());
-        assertEquals("oPARK00501", path_temp1.get(4).getID());
+        // Switching Algorithm by calling the setter
+        // in this case, switch to Astar
+        AStarManager asm = new AStarManager(mdb);
+        context.setPathfindingAlgo(asm);
+        LinkedList<AlgoNode> path_temp1 = context.getPath("oPARK01001","oPARK00501");
 
-        // a NullPointerException will be caught and have system print.
-        LinkedList<AlgoNode> path_temp2 = dsm.getPath("oPARK01101", "oPARK00801");
+        assertEquals(6, path_temp1.size());
+        assertEquals("oPARK01001", path_temp1.get(0).getID());
+        assertEquals("oPARK00801", path_temp1.get(1).getID());
+        assertEquals("oPARK00701", path_temp1.get(2).getID());
+        assertEquals("oPARK00401", path_temp1.get(3).getID());
+        assertEquals("oPARK00601", path_temp1.get(4).getID());
+        assertEquals("oPARK00501", path_temp1.get(5).getID());
 
-
-        assertNull(path_temp2);
-
-
-        LinkedList<AlgoNode> path_temp3 = dsm.getPath("oPARK00401","oPARK00501");
-        assertEquals(3, path_temp3.size());
-        assertEquals("oPARK00401", path_temp3.get(0).getID());
-        assertEquals("oPARK00601", path_temp3.get(1).getID());
-        assertEquals("oPARK00501", path_temp3.get(2).getID());
-
-        System.out.println("If reach here tests for DFSManager.getPath() are passed!!");
-
+        System.out.println("If reach here, tests for Context.getPath() are passed!! ");
     }
+
 }
