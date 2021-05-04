a25b7f1800f9b9ef2cbb0670e27e04b5e9a502fb
duffelmuffinvii <30965453+duffelmuffinvii@users.noreply.github.com>
2021-05-04 19:05:48 -0400

    finally got rid of weird shadows on service request forms (#437)
    * got rid of weird shadows on service request pages

--- src/main/java/edu/wpi/teamo/views/serviceRequestHubPage.java
+++ src/main/java/edu/wpi/teamo/views/serviceRequestHubPage.java
@@ -184,7 +184,6 @@ public class serviceRequestHubPage implements Initializable {
         JFXDialog errorWindow = new JFXDialog(stackPane, content, JFXDialog.DialogTransition.TOP);
 
         JFXButton closeButton = new JFXButton(App.resourceBundle.getString("key.close"));
-        closeButton.setStyle("-fx-background-color: #F40F19; -fx-text-fill: #fff");
         closeButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
